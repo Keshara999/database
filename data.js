5cb59c7e0e82277c712af09c8830fd783691016e
Keshara999 <kesharsyvdheh38@gmail.com>
2025-03-31 17:35:59 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+{
+"zipmegalink": "https://mega.nz/file/w1YDkSCa#OiEs8jkTVSOlPOrxKBB9-PilUFx2sma14mKOot_FkzI",
+"update": "https://mega.nz/file/g9REhCLA#JmjNN1BbHjwZVQCFWZR84JbE918xKmTuNQf6BooHpGc",
+"join": "https://chat.whatsapp.com/Eu4D6iR5Wjs40e4al8nH40"
+}
